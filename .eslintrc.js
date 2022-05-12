2ad878ab625386cc30941849e39e9712cd277c34
Ivan Nikolaiev <nikolaiev.i03@gmail.com>
2022-05-12 13:08:22 +0300

    add rule about singlequote

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,13 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
-    'quotes': ['error', 'single']
+    "@typescript-eslint/quotes": [
+      "error",
+      "single",
+      {
+        "avoidEscape": true,
+        "allowTemplateLiterals": true
+      }
+    ]
   },
 };
