f19c84ebc8d233944431ca6631c2bc26fc937324
Ivan Nikolaiev <nikolaiev.i03@gmail.com>
2022-05-12 13:03:49 +0300

    change eslint, add gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -7,4 +7,4 @@ import { AppService } from './app.service';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
