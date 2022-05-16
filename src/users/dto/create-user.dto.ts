f37772d864f15021c54d28c11874bf78c553d3fd
Oleksandr <OlexandrTanasienko@gmail.com>
2022-05-16 15:07:19 +0300

    Create dto for user creation

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,13 @@
+export class CreateUserDto {
+  readonly name: string;
+
+  readonly surname: string;
+
+  readonly email: string;
+
+  readonly birthDate: Date;
+
+  readonly address: string;
+
+  readonly sex: string;
+}
