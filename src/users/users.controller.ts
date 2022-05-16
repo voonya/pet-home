54a411e337739bc86b5992b3d0ab99da65a50e4f
Oleksandr <OlexandrTanasienko@gmail.com>
2022-05-16 15:12:25 +0300

    Write methods for user creation

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
+  @Post()
+  create(@Body() userDto: CreateUserDto) {
+    return this.usersService.createUser(userDto);
+  }
 
   @Get()
   getAll() {
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
   getAll() {
     return [];
+  }
+
+  createUser(userDto: CreateUserDto) {
+    return userDto;
   }
 }
