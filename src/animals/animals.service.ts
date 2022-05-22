c9430516b2ebffea0f48fd03059db32889a28a37
Oleksandr <OlexandrTanasienko@gmail.com>
2022-05-23 02:05:31 +0300

    Mock animals and use array in animal service

--- src/animals/animals.service.ts
+++ src/animals/animals.service.ts
@@ -3,40 +3,38 @@ import { CreateAnimalDto } from './dto/create-animal.dto';
 import { AnimalDto } from './dto/animal.dto';
 import { v4 as uuidv4 } from 'uuid';
 import { UpdateAnimalDto } from './dto/update-animal.dto';
+import { animals } from './animals';
 
 @Injectable()
 export class AnimalsService {
   getAll(): AnimalDto[] {
-    return [];
+    return animals;
   }
 
   createAnimal(createAnimalDto: CreateAnimalDto): AnimalDto {
-    return { id: uuidv4(), ...createAnimalDto };
+    const newAnimal: AnimalDto = { id: uuidv4(), ...createAnimalDto };
+    animals.push(newAnimal);
+    return newAnimal;
   }
 
   getById(id: string): AnimalDto {
-    return {
-      breed: '',
-      description: '',
-      id: id,
-      name: '',
-      ownerId: '',
-      type: undefined,
-    };
+    return animals.find((animal) => animal.id === id);
   }
 
-  update(id: string, animalDto: UpdateAnimalDto): AnimalDto {
-    return { id: id, ownerId: '', ...animalDto };
+  update(id: string, updateAnimalDto: UpdateAnimalDto): AnimalDto {
+    const oldAnimal = animals.find((animal) => animal.id === id);
+    const index = animals.indexOf(oldAnimal);
+    const newAnimal = { ...oldAnimal, ...updateAnimalDto };
+    animals[index] = newAnimal;
+    return newAnimal;
   }
 
   remove(id: string): AnimalDto {
-    return {
-      breed: 'some',
-      description: '',
-      id: id,
-      name: '',
-      ownerId: '',
-      type: undefined,
-    };
+    const animalToRemove = animals.find((animal) => animal.id === id);
+    const index = animals.indexOf(animalToRemove);
+    if (index !== -1) {
+      animals.splice(index, 1);
+    }
+    return animalToRemove;
   }
 }
---
+++ src/animals/animals.ts
@@ -0,0 +1,37 @@
+import { AnimalDto } from './dto/animal.dto';
+import { AnimalType } from './animal-type';
+
+export const animals: AnimalDto[] = [
+  {
+    id: '039c2db2-da21-11ec-9d64-0242ac120002',
+    name: 'Bayraktar',
+    ownerId: '123e4567-e89b-12d3-a456-426614174000',
+    type: AnimalType.Dog,
+    breed: 'Husky',
+    description: 'The best dog',
+  },
+  {
+    id: '039c3118-da21-11ec-9d64-0242ac120002',
+    name: 'Bayraktar',
+    ownerId: '0bdc5c68-da0d-11ec-9d64-0242ac120002',
+    type: AnimalType.Cat,
+    breed: 'Husky',
+    description: 'The best cat',
+  },
+  {
+    id: '039c3226-da21-11ec-9d64-0242ac120002',
+    name: 'Pirate',
+    ownerId: '122fe7b0-da0d-11ec-9d64-0242ac120002',
+    type: AnimalType.Parrot,
+    breed: 'Husky',
+    description: 'The best pet',
+  },
+  {
+    id: '039c3320-da21-11ec-9d64-0242ac120002',
+    name: 'Bobik',
+    ownerId: '122fe904-da0d-11ec-9d64-0242ac120002',
+    type: AnimalType.Dog,
+    breed: 'SImple',
+    description: 'The best dog',
+  },
+];
