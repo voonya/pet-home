83cead54ab86a40d67e44f0a7eb616962944b417
Oleksandr <OlexandrTanasienko@gmail.com>
2022-05-23 00:09:16 +0300

    Create empty module, controller and service for animals

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { AnimalsModule } from './animals/animals.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AnimalsModule],
 })
 export class AppModule {}
