d1fb0f82f80862cbfff09b4135cea3dcde693651
Oleksandr <OlexandrTanasienko@gmail.com>
2022-05-23 19:48:08 +0300

    Create pagination middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { UsersModule } from '@users/users.module';
 import { AnimalsModule } from '@animals/animals.module';
+import { PagingMiddleware } from 'middlewares/paging.middleware';
 
 @Module({
   imports: [UsersModule, AnimalsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(PagingMiddleware);
+  }
+}
