9b69cf59980cf818bbadf59c0f3d244330c55ac7
Ivan Nikolaiev <nikolaiev.i03@gmail.com>
2022-05-24 14:49:13 +0300

    add feedback module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,22 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { FeedbackController } from '@feedback/feedback.controller';
-import { FeedbackService } from '@feedback/feedback.service';
-import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { ForecastModule } from '@forecast/forecast.module';
 import { UsersModule } from '@users/users.module';
 import { AnimalsModule } from '@animals/animals.module';
 import { PagingMiddleware } from 'middlewares/paging.middleware';
-
+import { FeedbackModule } from '@feedback/feedback.module';
 @Module({
   imports: [
-    HttpModule,
     ConfigModule.forRoot({
       envFilePath: 'src/.env',
     }),
     ForecastModule,
     UsersModule,
     AnimalsModule,
+    FeedbackModule,
   ],
-  controllers: [FeedbackController],
-  providers: [FeedbackService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
