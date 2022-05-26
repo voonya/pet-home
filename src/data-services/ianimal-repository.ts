9834941a7e9443f861db1b6d9ed9f8f3aaadb1ff
Oleksandr <OlexandrTanasienko@gmail.com>
2022-05-26 16:26:54 +0300

    Make animal interface

--- src/data-services/ianimal-repository.ts
+++ src/data-services/ianimal-repository.ts
@@ -1,22 +1,13 @@
 import { AnimalDto } from 'animals/dto';
 
-export abstract class IAnimalRepository {
-  abstract getAll(userId: string): Promise<AnimalDto[]>;
+export interface IAnimalRepository {
+  getAll(userId: string): Promise<AnimalDto[]>;
 
-  abstract getById(
-    id: string,
-    userId: string,
-  ): Promise<AnimalDto | null | undefined>;
+  getById(id: string, userId: string): Promise<AnimalDto | null | undefined>;
 
-  abstract create(dto: AnimalDto): Promise<AnimalDto>;
+  create(dto: AnimalDto): Promise<AnimalDto>;
 
-  abstract update(
-    id: string,
-    dto: AnimalDto,
-  ): Promise<AnimalDto | null | undefined>;
+  update(id: string, dto: AnimalDto): Promise<AnimalDto | null | undefined>;
 
-  abstract remove(
-    id: string,
-    userId: string,
-  ): Promise<AnimalDto | null | undefined>;
+  remove(id: string, userId: string): Promise<AnimalDto | null | undefined>;
 }
