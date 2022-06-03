9bd0181a5fb72716acf22159c576286b2b0302f3
Ivan <nikolaiev.i03@gmail.com>
2022-06-04 00:02:30 +0300

    fix

--- src/common/models/forecast/forecast.controller.ts
+++ src/common/models/forecast/forecast.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { ForecastService } from 'common/models/forecast/forecast.service';
+import { JwtAuthGuard } from 'auth/guards/jwt-auth.guard';
 
 @Controller('forecast')
+@UseGuards(JwtAuthGuard)
 export class ForecastController {
   constructor(private forecast: ForecastService) {}
 
